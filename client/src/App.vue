4aa712e18879a80d2098cc5417da7aaaf0f2105b
Dmitriy Pochodnya <dmitriy.troy@gmail.com>
2017-09-07 00:21:18 +0300

    add registration routes and page

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,12 +11,28 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  html {
+    box-sizing: border-box;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+  }
+
+  h1 {
+    margin: 0 0 0.5em;
+    font-size: 30px;
+    text-align: center;
+  }
+
 </style>
