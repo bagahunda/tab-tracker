5858711bce8c9ba08948645fa1d9badb113f2faa
Dmitriy Pochodnya <dmitriy.troy@gmail.com>
2017-09-08 14:46:18 +0300

    add login/register logic

--- client/src/App.vue
+++ client/src/App.vue
@@ -35,4 +35,77 @@ export default {
     text-align: center;
   }
 
+  .register {
+    padding: 30px 0;
+    display: flex;
+    height: 100vh;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .logo {
+    margin-bottom: 2em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .logo svg {
+    width: 80px;
+    fill: #ff54a6;
+  }
+
+  .logo span {
+    position: relative;
+    margin-top: -0.5em;
+    font-size: 35px;
+    line-height: 1;
+    letter-spacing: 4px;
+    text-transform: uppercase;
+    font-weight: bold;
+    background-color: #fff;
+    z-index: 2;
+    transform: rotate(-10deg);
+  }
+
+  .register__form {
+    display: flex;
+    width: 100%;
+    max-width: 400px;
+    flex-direction: column;
+  }
+
+  .register__form input {
+    height: 40px;
+    margin-bottom: 1em;
+    padding: 0 0.5em;
+    outline: none;
+    font-size: 14px;
+    border: 1px solid #2c3e50;
+    border-radius: 3px;
+    transition: all 0.3s ease-in-out;
+  }
+
+  .register__form input:focus {
+    border: 1px solid #ff54a6;
+  }
+
+  .register__form button {
+    height: 40px;
+    appearance: none;
+    border: none;
+    font-size: 14px;
+    text-transform: uppercase;
+    color: #fff;
+    outline: none;
+    background-color: #ff54a6;
+    border-radius: 3px;
+    font-weight: bold;
+  }
+
+  .error {
+    color: red;
+  }
+
 </style>
--- client/src/main.js
+++ client/src/main.js
@@ -2,14 +2,24 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Header from './components/Header.vue'
 import router from './router'
+import { sync } from 'vuex-router-sync'
+import store from '@/store/store'
 
 Vue.config.productionTip = false
+
+sync(store, router)
+
+Vue.component('app-header', Header)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
-  components: { App }
+  components: {
+    App
+  }
 })
